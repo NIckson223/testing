c03d0a89bad09dcbe8b80ddd49deea641bab5220
NIckson223 <nicktyshko23@gmai.com>
2023-09-17 13:59:55 +0300

    Create project

---
+++ main.py
@@ -0,0 +1,11 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget,
+                            QLabel, QHBoxLayout,QVBoxLayout)
+
+app = QApplication([])
+main_win = QWidget()
+main_win.setWindowTitle("Тестування")
+main_win.resize(400,400)
+
+main_win.show()
+app.exec_()
